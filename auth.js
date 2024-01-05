45f8aea7d6025ffd6ffe4ccd54d63e459b0e8e28
H-M-Nizum <hmnizum1714032@gmail.com>
2024-01-05 21:40:30 +0600

    4 : Logout 27

--- auth.js
+++ auth.js
@@ -74,3 +74,22 @@ const handleLogin = (event) => {
         });
     }
   };
+
+  // user logout
+  const handlelogOut = () => {
+    const token = localStorage.getItem("token");
+  
+    fetch("https://testing-8az5.onrender.com/patient/logout", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.removeItem("token");
+        localStorage.removeItem("user_id");
+      });
+  };
